34738d31e5703fa825fd2d685d94eb3a50b76f5e
Spotandjake <spotandjake@hotmail.com>
2023-04-03 09:23:38 -0400

    feat: Update Next

--- src/Foreground/next.config.js
+++ src/Foreground/next.config.js
@@ -1,13 +1,12 @@
 // next.config.js
 module.exports = {
   swcMinify: true,
-  modern: true,
   basePath: '/_ext/Foreground',
   optimizeFonts: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
-  experimental: {
-    workerThreads: true
-  }
+  // experimental: {
+  //   workerThreads: true
+  // }
 };
